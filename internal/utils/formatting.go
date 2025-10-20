099d41f9fc3428a890da21c35e61fa66ee8741f8
Juanma Roca <juanmaxroca@gmail.com>
2025-10-20 14:57:45 -0400

    opt[utils]: centralized string formatting helper functions

---
+++ internal/utils/formatting.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/rocajuanma/palantir"
+)
+
+func BoldText(text string, color string) string {
+	if color == "" {
+		color = palantir.ColorBold
+	}
+	return fmt.Sprintf("%s%s%s", color, text, palantir.ColorReset)
+}
+
+func ColorSectionHeader(text string) string {
+	return fmt.Sprintf("%s%s%s", palantir.ColorBold+palantir.ColorCyan, text, palantir.ColorReset)
+}
+
+func ColorAppName(text string) string {
+	return fmt.Sprintf("%s%s%s", palantir.ColorGreen, text, palantir.ColorReset)
+}
+
+func ColorGroupNameWithIcon(text string) string {
+	return fmt.Sprintf("%s 📁", BoldText(text, ""))
+}
+
+func ColoredName(text string, color string) string {
+	return BoldText(text, color)
+}
